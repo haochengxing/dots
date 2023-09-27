a851628dc2053fc9bca4b67afb743587c68377a0
LONGTU\chengxinghao <haochengxing2006@163.com>
2023-09-27 18:58:20 +0800

    堆排序

--- Assets/algorithm/sort2/sort2.cs
+++ Assets/algorithm/sort2/sort2.cs
@@ -7,11 +7,20 @@ public class sort2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int[] array = { 9, 5, 8, 7, 2, 6, 4, 1, 3 };
+        //int[] array = { 9, 5, 8, 7, 2, 6, 4, 1, 3 };
 
-        quickSort(array, 0, array.Length - 1);
+        //quickSort(array, 0, array.Length - 1);
 
-        Debug.Log(string.Join(",", array));
+        //Debug.Log(string.Join(",", array));
+
+
+        int[] array2 = {0, 9, 5, 8, 7, 2, 6, 4, 1, 3 };
+
+        HeapSort(array2, array2.Length-1);
+
+        array2[0] = 0;
+
+        Debug.Log(string.Join(",", array2));
     }
 
     // Update is called once per frame
@@ -81,4 +90,48 @@ public class sort2 : MonoBehaviour
         return partition(array, p,r);
 
     }
+
+
+    void Heapify(int[] array, int v, int n)
+    {
+        int i, j;
+        i = v; j = 2 * i;
+        array[0] = array[i];
+        while (j <= n)
+        {
+            if (j < n && array[j] < array[j + 1])
+            {
+                j++;
+            }
+            if (array[0] < array[j])
+            {
+                array[i] = array[j]; 
+                i = j;
+                j = 2 * i;
+            }
+            else
+            {
+                j = n + 1;
+            }
+        }
+        array[i] = array[0];
+    }
+
+    void HeapSort(int[] array, int n)
+    {
+        int i;
+        for (i = n / 2; i >= 1; i--)
+        {
+            Heapify(array, i, n);
+        }
+        for (i = n; i > 1; i--)
+        {
+            array[0] = array[i];
+            array[i] = array[1];
+            array[1] = array[0];
+            Heapify(array, 1, i - 1);
+        }
+    }
+
+
 }
